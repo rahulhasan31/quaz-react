7405548ad514879b2e08c0620b6ddb47cdfe63f3
rahulhasan31 <rahulhasan666@gmail.com>
2022-10-12 22:46:48 +0600

    added

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-        <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand as={Link} to='/'>Quiz Game</Navbar.Brand>
+
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand  as={Link}to='/'>Quiz Game</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
             <Nav.Link as={Link} to='/statistics'>Statistics</Nav.Link>
+           
           </Nav>
-        </Container>
-      </Navbar>
+          <Nav>
+           
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
       
         </div>
